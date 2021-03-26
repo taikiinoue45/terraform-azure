9fc72987dae00daa9a0989a9d93f4b4af8d5d868
taikiinoue45 <taikiinoue45@gmail.com>
2021-03-26 10:35:17 +0900

    Initial commit

--- reception/func/__init__.py
+++ reception/func/__init__.py
@@ -1,6 +1,7 @@
 import logging
+
 import azure.functions as func
 
 
-def main(myblob: func.InputStream):
-    logging.info('Python Blob trigger function processed %s', myblob.name)
+def main(myblob: func.InputStream) -> None:
+    logging.info("Python Blob trigger function processed %s", myblob.name)
