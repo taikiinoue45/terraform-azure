e9ec1122b582936266672385dd0d9f6d4f4a7631
taikiinoue45 <taikiinoue45@gmail.com>
2021-03-26 10:46:00 +0900

    Initial commit

--- reception/infra/data.tf
+++ reception/infra/data.tf
@@ -3,11 +3,11 @@ data "azurerm_resource_group" "rg" {
 }
 
 data "azurerm_storage_account" "storage" {
-  name                = "storage20210324165333"
+  name                = "storage20210326102834"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 data "azurerm_container_registry" "registry" {
-  name                = "registry20210324165333"
+  name                = "registry20210326102834"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
