06a1309a075c88f6b7f555fde41a7c5b6feeeae2
taikiinoue45 <taikiinoue45@gmail.com>
2021-03-26 10:58:36 +0900

    Initial commit

--- reception/infra/main.tf
+++ reception/infra/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 resource "azurerm_storage_container" "container" {
   name                  = "${var.prefix}"
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_name  = data.azurerm_storage_account.storage.name
   container_access_type = "private"
 }
 
